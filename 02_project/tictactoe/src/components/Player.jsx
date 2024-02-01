4fac4eab5b4606acf335c916a2a37ad506cbc70a
geochrs <geo.christopoulos@hotmail.com>
2024-02-01 23:59:02 +0200

    final files

--- 02_project/tictactoe/src/components/Player.jsx
+++ 02_project/tictactoe/src/components/Player.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useState } from "react"
 
-export default function Player({ initialName, symbol }) {
-    const [playerName, setPlayerName] = useState(initialName);
+export default function Player({ initialName, symbol, isActive, onChangeName }) {
+
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     const handleEditClick = () => {
-        setIsEditing(prevEditing => !prevEditing);
+        setIsEditing((prevEditing => !prevEditing));
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     const handleChange = (evt) => {
@@ -17,11 +21,11 @@ export default function Player({ initialName, symbol }) {
 
     if (isEditing) {
         editablePlayerName = (<input type="text" value={playerName} onChange={handleChange} required />)
-        btnCaption = 'Save'
+        btnCaption = 'Save';
     }
 
     return (
-        <li>
+        <li className={isActive ? 'active' : undefined}>
             <span className="player">
                 {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
